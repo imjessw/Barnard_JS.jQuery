779db1e5af190081dba2f42c218c73be3e18e89e
imjessw <imjessw@gmail.com>
2014-10-08 20:45:22 -0400

    walking through hide show with class

--- Week2StarTracker/chapter_02/01_document_ready/script.js
+++ Week2StarTracker/chapter_02/01_document_ready/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
   console.log("OMG JAVASCRIPT");
-    $('#celebs tbody tr:even').addClass('zebra');
+    $('#celebs tr:even').addClass('zebra');
+
+		$('#toggleButton').click(function(){
+			$('#disclaimer').toggle();
+			if ($('#disclaimer').is(':visible')) {
+				$(this).val('hide');
+			}
+			else{
+				$(this).val('show');
+			}
+		})	
+
+
 });
